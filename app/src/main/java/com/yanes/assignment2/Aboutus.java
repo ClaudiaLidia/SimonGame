1a47169c99af02bda564d6d68457cd5a06c42a20
lyanesgarcia <lyanesgarcia@my.apsu.edu>
2018-03-12 00:28:41 -0600

    Update of about us activity

--- app/src/main/java/com/yanes/assignment2/Aboutus.java
+++ app/src/main/java/com/yanes/assignment2/Aboutus.java
@@ -1,8 +1,15 @@
 package com.yanes.assignment2;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by LidiayClaudia on 12/03/2018.
  */
 
-public class Aboutus {
+public class Aboutus extends Activity {
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aboutus);
+    }
 }
