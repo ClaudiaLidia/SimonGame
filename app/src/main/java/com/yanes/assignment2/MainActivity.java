cfc003545cd713b7d2ea5b3506b62b251c0b8f98
cyanesgarcia <cyanesgarcia@my.apsu.edu>
2018-03-12 10:00:53 +0100

    Add the activities for the 3 variations of the game

--- app/src/main/java/com/yanes/assignment2/MainActivity.java
+++ app/src/main/java/com/yanes/assignment2/MainActivity.java
@@ -1,12 +1,18 @@
 package com.yanes.assignment2;
+/**
+ * Claudia and Lidia Yanes Garcia
+ * Assignment 2
+ * CSCI 4020
+ **/
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends Activity  implements View.OnClickListener {
-
+    private static final int REQUEST_CODE_ADD = 100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +27,15 @@ public class MainActivity extends Activity  implements View.OnClickListener {
 
     @Override
     public void onClick(View view) {
+        Intent intent = null;
+        if (view.getId() == R.id.bfirstv) {
+            intent = new Intent(this, Firstv.class);
+        } else if (view.getId() == R.id.bsecondv) {
+            intent = new Intent(this, Secondv.class);
+        } else if (view.getId() == R.id.bthirdv) {
+            intent = new Intent(this, Thirdv.class);
+        }
+        startActivityForResult(intent, REQUEST_CODE_ADD);
         
     }
 }
