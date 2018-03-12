d0349d2b82be4fc8f8386aeaa530a157baa36d3e
cyanesgarcia <cyanesgarcia@my.apsu.edu>
2018-03-12 23:28:48 +0100

    Create credits activity

---
+++ app/src/main/java/com/yanes/assignment2/Credits.java
@@ -0,0 +1,15 @@
+package com.yanes.assignment2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by LidiayClaudia on 12/03/2018.
+ */
+
+public class Credits extends Activity {
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credits);
+    }
+}
